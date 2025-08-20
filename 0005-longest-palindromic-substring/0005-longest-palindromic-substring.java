a921ae7b079e58f7991112accf840093b5b756fa
Samya-das06 <156400163+Samya-das06@users.noreply.github.com>
2025-08-20 23:21:40 +0530

    Time: 8 ms (97.3%), Space: 43.2 MB (52.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,23 +1,43 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        if (s == null || s.length() < 1) return "";
+        char[] t = preprocess(s);
+        int[] p = new int[t.length];
+        int center = 0, right = 0;
+        int maxLen = 0, centerIndex = 0;
+
+        for (int i = 1; i < t.length - 1; i++) {
+            int mirror = 2 * center - i;
+            if (right > i)
+                p[i] = Math.min(right - i, p[mirror]);
+
+            while (t[i + 1 + p[i]] == t[i - 1 - p[i]])
+                p[i]++;
+
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIndex = i;
             }
         }
-        return s.substring(start, end + 1);
+
+        int start = (centerIndex - maxLen) / 2;
+        return s.substring(start, start + maxLen);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private char[] preprocess(String s) {
+        char[] ret = new char[s.length() * 2 + 3];
+        ret[0] = '^';
+        for (int i = 0; i < s.length(); i++) {
+            ret[2 * i + 1] = '#';
+            ret[2 * i + 2] = s.charAt(i);
         }
-        return right - left - 1;
+        ret[ret.length - 2] = '#';
+        ret[ret.length - 1] = '$';
+        return ret;
     }
 }
