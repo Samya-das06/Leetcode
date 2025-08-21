d1327a235539f75fd8d9571cce3bdeccbb713db7
Samya-das06 <156400163+Samya-das06@users.noreply.github.com>
2025-08-21 20:00:49 +0530

    Time: 140 ms (5.14%), Space: 46.5 MB (5.48%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
@@ -2,39 +2,20 @@ public class Solution {
     public int numSubmat(int[][] mat) {
         int m = mat.length, n = mat[0].length;
         int res = 0;
-        int[][] h = new int[m][n];
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                h[i][j] = mat[i][j] == 0 ? 0 : (i == 0 ? 1 : h[i-1][j] + 1);
+        int[] height = new int[n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                height[j] = mat[i][j] == 0 ? 0 : height[j] + 1;
             }
-        }
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                int min = h[i][j];
-                for(int k = j; k >= 0 && min > 0; --k){
-                    min = Math.min(min, h[i][k]);
-                    res += min;
+            for (int j = 0; j < n; ++j) {
+                if (height[j] == 0) continue;
+                int minH = height[j];
+                for (int k = j; k >= 0 && height[k] > 0; --k) {
+                    minH = Math.min(minH, height[k]);
+                    res += minH;
                 }
             }
         }
         return res;
-    }
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        int[][] mat1 = {
-            {1,0,1},
-            {1,1,0},
-            {1,1,0}
-        };
-
-        int[][] mat2 = {
-            {0,1,1,0},
-            {0,1,1,1},
-            {1,1,1,0}
-        };
-
-        System.out.println(sol.numSubmat(mat1));
-        System.out.println(sol.numSubmat(mat2));
     }
 }
