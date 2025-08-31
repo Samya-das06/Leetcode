fe94602627ceae630655ee99fbabd3399183f114
Samya Das <156400163+Samya-das06@users.noreply.github.com>
2025-08-31 20:09:55 +0530

    Time: 207 ms (60.77%), Space: 41.9 MB (7.1%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        for (int c = 0; c < 9; c++) {
+            if (board[row][c] == num) return false;
+        }
+        for (int r = 0; r < 9; r++) {
+            if (board[r][col] == num) return false;
+        }
+        int boxRowStart = (row / 3) * 3;
+        int boxColStart = (col / 3) * 3;
+        for (int r = boxRowStart; r < boxRowStart + 3; r++) {
+            for (int c = boxColStart; c < boxColStart + 3; c++) {
+                if (board[r][c] == num) return false;
+            }
+        }
+        return true;
+    }
+}
