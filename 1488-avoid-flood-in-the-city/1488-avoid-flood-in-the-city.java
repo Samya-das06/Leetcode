200c5c0ea3b84d454612426e56c4c19d13915805
Samya Das <156400163+Samya-das06@users.noreply.github.com>
2025-10-07 09:45:30 +0530

    Time: 63 ms (94.52%), Space: 62.9 MB (23.29%) - LeetHub

---
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        int n = rains.length;
+        int[] res = new int[n];
+        Map<Integer, Integer> fullLakes = new HashMap<>();
+        TreeSet<Integer> dryDays = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            if (rains[i] == 0) {
+                dryDays.add(i);
+                res[i] = 1;
+            } else {
+                int lake = rains[i];
+                if (fullLakes.containsKey(lake)) {
+                    int lastRainDay = fullLakes.get(lake);
+                    Integer dryDay = dryDays.higher(lastRainDay);
+                    if (dryDay == null) {
+                        return new int[0];
+                    }
+                    res[dryDay] = lake;
+                    dryDays.remove(dryDay);
+                }
+                fullLakes.put(lake, i);
+                res[i] = -1;
+            }
+        }
+        return res;
+    }
+}
