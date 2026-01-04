b12f8580cabda2ef4b40af3c3ea1cb5f5073f22e
Samya Das <156400163+Samya-das06@users.noreply.github.com>
2026-01-04 13:18:23 +0530

    Time: 8 ms (98.74%), Space: 46.3 MB (68.18%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int totalSum = 0;
+        
+        for (int num : nums) {
+            int foundDivisor = 0;
+            
+            for (int divisor = 2; divisor * divisor <= num; divisor++) {
+                if (num % divisor == 0) {
+                    if (foundDivisor == 0) {
+                        foundDivisor = divisor;
+                    } else {
+                        foundDivisor = 0;
+                        break;
+                    }
+                }
+            }
+            
+            if (foundDivisor > 0 && foundDivisor != num / foundDivisor) {
+                totalSum += 1 + num + foundDivisor + num / foundDivisor;
+            }
+        }
+        
+        return totalSum;
+    }
+}
